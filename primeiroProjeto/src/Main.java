6e2d5a1f08eb7f0c783962390fcc86aa5af01a84
wesleycristian <soldadocristiam@hotmail.com>
2021-09-07 17:43:20 -0300

    primeiro commit

---
+++ primeiroProjeto/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        int a= 10;
+        int b= 9;
+        int c= 6;
+        a=3;
+        int x= a+b-c;
+        System.out.println(x);
+        System.out.println(c+a);
+    }
+}
