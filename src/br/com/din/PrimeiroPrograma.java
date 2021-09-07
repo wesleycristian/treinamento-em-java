253b6e7b0e1f098cf34e5f88a2fd36d9586c27f9
wesleycristian <soldadocristiam@hotmail.com>
2021-09-07 20:17:54 -0300

    forst commit 2

---
+++ src/br/com/din/PrimeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.din;
+
+import br.com.din.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato= new Gato();
+
+        System.out.println(gato);
+        /*int a = 5;
+        int b = 6;
+        System.out.println("Hello World" + (a+b));*/
+    }
+}
+
